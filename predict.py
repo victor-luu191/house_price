2d1dab3481a551b3dfc442617efc2700dc0f70cd
victor <victor.md.luu@global-fashion-group.com>
2018-07-01 17:10:35 +0800

    add class `Predictor`

---
+++ predict.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.externals import joblib
+
+from setting import *
+import os
+
+
+class Predictor():
+    def __init__(self, model, features):
+        self.model = model
+        self.features = features
+
+    def predict(self, X):
+        feats = self.features
+        return self.model.predict(X[feats])
+
+
+def load_predictor(fname):
+    predictor = joblib.load(fname)
+    print('the predictor was trained using features {}'.format(predictor.features))
+    return predictor
+
+
+def load_test_data():
+    print('Load test data')
+    input_file = os.path.join(DAT_DIR, 'data_all.csv')
+    data_all = pd.read_csv(input_file)
+    X_test = data_all[data_all['SalePrice'].isnull()]
+    return X_test
+
+
+if __name__ == '__main__':
+    fname = os.path.join(MODEL_DIR, 'xgboost.pkl') # random_forest
+    predictor = load_predictor(fname)
+
+    X_test = load_test_data()
+
+    X_test['SalePrice'] = predictor.predict(X_test)
+    X_test.drop_duplicates(inplace=True)
+    pred_file = os.path.join(RES_DIR, 'predictions.csv')
+    X_test.to_csv(pred_file, index=False)
+
+    print('make submission file')
+    cols = ['Id', 'SalePrice']
+    submit = X_test[cols].drop_duplicates() # todo check why duplicates occur
+    submit.to_csv(os.path.join(RES_DIR, 'submit.csv'), index=False)
