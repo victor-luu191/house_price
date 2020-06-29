45fca7a502877a76943ff283e2a6704f0e513061
Minh-Duc, LUU <victor.md.luu@gmail.com>
2020-06-29 09:32:59 -0700

    Update plot_utils.py
    change hardcoded values to parameters

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -7,7 +7,7 @@ cmap = sns.diverging_palette(220, 10, as_cmap=True)
 RED_BLUE = plt.get_cmap('RdBu')
 
 
-def plot_corr_matrix(data, annotate=False, color_map=RED_BLUE):
+def plot_corr_matrix(data, annotate=False, color_map=RED_BLUE, figsize=(10, 10), vmax=.3, center=0):
     sns.set_style('white')
     # Compute the correlation matrix
     corr = data.corr()
@@ -16,12 +16,12 @@ def plot_corr_matrix(data, annotate=False, color_map=RED_BLUE):
     mask = np.triu(np.ones_like(corr, dtype=np.bool))
 
     # Set up the matplotlib figure
-    f, ax = plt.subplots(figsize=(11, 9))
+    f, ax = plt.subplots(figsize=figsize)
 
     # Draw the heatmap with the mask and correct aspect ratio
     sns.heatmap(corr, mask=mask,
                 cmap=color_map, cbar_kws={"shrink": .5},
-                vmax=.3, center=0,
+                vmax=vmax, center=center,
                 square=True, linewidths=.5,
                 annot=annotate
                 )
